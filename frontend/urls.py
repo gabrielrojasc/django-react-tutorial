090a8f5729a4ae07374085ce6ff68a1835c4a436
Gabriel Rojas <gabo06123@gmail.com>
2022-06-12 20:44:26 -0400

    feat: created react apss and homepage route

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 
 from .views import index
 
-urlpatterns = [path("", index)]
+urlpatterns = [
+    path("", index),
+    path("join/", index),
+    path("create/", index),
+]
